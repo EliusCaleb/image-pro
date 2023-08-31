b109f9ebfeded1365082f13a52ff92e594317725
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-31 03:48:43 +0300

    added vite,tailwind and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {Routes,Route} from 'react-router-dom'
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+    </>
+  )
+}
+
+export default App
