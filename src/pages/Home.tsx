c4b9934c38bc4c449d2a80ea67808e570e87ba64
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-31 04:17:22 +0300

    import the  component to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
+import ImageGallery from "../components/ImageGallery"
+import Navbar from "../components/Navbar"
+import UploadForm from "../components/UploadForm"
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="max-w-4xl mx-auto mt-4">
+        <Navbar/>
+        <UploadForm/>
+        <ImageGallery/>
+    </div>
   )
 }
 
