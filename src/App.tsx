2fbf8e8250c63eb014ec2c2aee573dc2889072cd
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-01 13:12:19 +0300

    incoporate protect and public routes into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import {Routes,Route} from 'react-router-dom'
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import PublicRoutes from "./routes/PublicRoutes";
+import PrivateRoutes from "./routes/PrivateRoutes";
 
 function App() {
   return (
     <>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/signup' element={<SignUp/>}/>
-        </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoutes>
+              <Home />
+            </PrivateRoutes>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoutes>
+              <SignUp />
+            </PublicRoutes>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
