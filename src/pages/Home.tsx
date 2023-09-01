cf800b91eb6359fbf7db19cdf82de01b04e0c487
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-01 13:10:28 +0300

    tested the global context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import ImageGallery from "../components/ImageGallery"
 import Navbar from "../components/Navbar"
 import UploadForm from "../components/UploadForm"
+import { useAuth } from "../hooks/useAuth"
+
 
 
 const Home = () => {
+  
+   const { user, isLoading } = useAuth()
+   console.log(user)
+   console.log(isLoading)
   return (
     <div className="max-w-4xl mx-auto mt-4">
         <Navbar/>
