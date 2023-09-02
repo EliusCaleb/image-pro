523e6c2ccb38b06a3eb7929b7bbab09ae5b041dc
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-02 08:40:54 +0300

    added functionality to the button

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
+import useStorage from "../hooks/useStorage"
 
 
 const UploadForm = () => {
+   const {startUpload,progress} = useStorage()
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const handleFileChanged = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    if(e.target.files && e.target.files[0])
+    setSelectedFile(e.target.files[0])
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
+     e.preventDefault()
+
+     if(selectedFile){
+      startUpload(selectedFile)
+     }
+     setSelectedFile(null)
+
+  }
   return (
     <div className="text-center mt-8">
-        <form className="flex items-center flex-col gap-8">
-        <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
-        <button className="btn gap-4">Upload
+        <form onSubmit={handleSubmit} className="flex items-center flex-col gap-8">
+        <input type="file" className="file-input file-input-bordered w-full max-w-xs"  onChange={handleFileChanged}/>
+        <button className={`btn gap-4  ${Boolean(progress) && 'loading'}` } disabled={!selectedFile}>Upload
           <span>🚀</span>
          </button>
         </form>
@@ -13,4 +31,4 @@ const UploadForm = () => {
   )
 }
 
-export default UploadForm
+export default UploadForm;
