f4723bc8eb0e7b1323ca9e80741c2dcd242a2b87
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-02 09:41:35 +0300

    make the components dynamics

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,18 +1,33 @@
+import useFirestore from "../hooks/useFirestore";
+
 const ImageGallery = () => {
+    const {docs: images, isLoading} = useFirestore('images');
+
+    if(isLoading){
+      return (
+        <div className="text-center mt-10">
+          <progress className="progress w-56"></progress>
+        </div>
+      )
+    }
+    
   return (
     <div className="grid md:grid-cols-3 justify-center mt-10 gap-4">
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure>
+     {images.map((image)=>(
+        <div key={image.imageUrl}   className="card card-compact w-full bg-white shadow-xl rounded-lg">
+        <figure className="max-h-[15rem] ">
           <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            src={image.imageUrl}
             alt="Shoes"
+            className="object-cover w-full h-full"
           />
         </figure>
-        <div className="card-body">
-          <p>Upload by:</p>
-           <span>Created on:</span>
+        <div className="card-body p-4">
+          <p className="text-gray-600">Upload by: {image.userEmail}:</p>
+           <span>Created on: {image.createdAt.toLocaleDateString()}</span>
         </div>
       </div>
+     ))}
     </div>
   );
 };
