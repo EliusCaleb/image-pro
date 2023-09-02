dece1c12f311b9acbed4e321bdba46d1ba9ee45e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-02 09:41:12 +0300

    create the  global context for adding images

---
+++ src/hooks/useFirestore.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { collection,query, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/config";
+import { orderBy } from "firebase/firestore/lite";
+
+type Image = {
+  createdAt: Date;
+  imageUrl: string;
+  userEmail: string;
+};
+const useFirestore = (collectionName: string) => {
+  const [docs, setDocs] = useState<Image[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+     let unsubscribe: () => void
+    const getData = async () => {
+      try {
+        const q = query(collection(db, collectionName), orderBy("createdAt", "desc"));
+
+         unsubscribe = onSnapshot(q, (querySnapshot) => {
+          const images: Image[] = [];
+          querySnapshot.forEach((doc) => {
+           
+            const imageUrl = doc.data().imageUrl
+            const createdAt = doc.data().createdAt.toDate()
+            const userEmail = doc.data().userEmail
+            images.push({imageUrl,createdAt,userEmail});
+          });
+          setDocs(images)
+          setIsLoading(false)
+        });
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false)
+      }
+      
+    };
+    getData()
+
+    return  () => unsubscribe && unsubscribe()
+  }, [collectionName]);
+  return {
+    docs,
+    isLoading,
+    
+  };
+};
+
+export default useFirestore;
